735a30a3efd2956e731fc98f5fa9e59b69923acf
Tosin Akinyele <tosinakinyele@Tosins-MacBook-Pro.local>
2020-03-26 20:53:08 +0100

    feat(backend and frontent): implement jwt auth

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'coffeeshop-ng', // the auth0 domain prefix
+    audience: 'coffeeshop.herokuapp.com', // the audience set for the auth0 app
+    clientId: 'OkkQUnF76y32Xx8hITzdjuUW9TGVtJDc', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
